399ce0df4aa174044ba112c7627aa76f816a792c
Chris Megrey <chris@megrey.com>
2024-05-31 14:41:38 -0700

    run black on settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    "django.contrib.auth.backends.ModelBackend",  
+    "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
